502e23d14e9312ba66ada3de2e50b34286754800
sparkmahadi <mahadi.development@gmail.com>
2024-02-08 11:56:34 +0600

    modified controller

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Todo } from './todo.model';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+    private todos: Todo[] = [];
+
+    @Get()
+    getAllTodos(): Todo[] {
+        return this.todos;
+    }
+
+    @Post()
+    createTodo(@Body() todo: Todo): Todo{
+        this.todos.push(todo);
+        return todo;
+    }
+
+    @Delete(':id')
+  deleteTodo(@Param('id') todoId: string): Todo {
+    const todoIndex = this.todos.findIndex(todo => todo.id === todoId);
+    const deletedTodo = this.todos[todoIndex];
+    this.todos.splice(todoIndex, 1);
+    return deletedTodo;
+  }
+  
+}
